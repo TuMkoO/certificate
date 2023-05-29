bf67271ad77a814f9f910b9ac33ae209f2699eb6
Timofey <mr.t09@mail.ru>
2023-05-29 16:45:58 +0300

    edit home component to ts

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,11 +4,14 @@
       <div class="py-4">
         <h4 class="text-center mb-4">Реестр сварочного производства</h4>
       </div>
+
       <app-loader v-if="loading"></app-loader>
+
       <certificates-filter
         v-if="!loading && storeData.length"
         v-model="filter"
       ></certificates-filter>
+
       <certificates-table
         v-if="!loading && storeData.length"
         :certificates="certificates"
@@ -26,97 +29,36 @@
   </app-page>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ref, onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import AppPage from "../components/ui/AppPage.vue";
 import AppLoader from "../components/ui/AppLoader.vue";
 import CertificatesFilter from "../components/certificates/CertificatesFilter.vue";
 import CertificatesTable from "../components/certificates/CertificatesTable.vue";
+import type { ICertificate } from "@/types/ICertificate";
 
-export default {
-  setup() {
-    //store
-    const store = useStore();
-    //loader
-    const loading = ref(false);
-    //
-    const filter = ref({});
-    //список удостоверений
-    const storeData = [];
+interface Filter {
+  firstname: string;
+  secondname: string;
+  numCertificate: string;
+  stigmaGeneral: string;
+  expiration: Date;
+  sortType: string;
+}
 
-    //загрузка списка удостоверений
-    onMounted(async () => {
-      loading.value = true;
-      await store.dispatch("certificate/load");
-      await store.dispatch("certificateSub/load");
+//store
+const store = useStore();
+//loader
+const loading = ref<boolean>(false);
+//поля фильтра
+const filter = ref<Filter>({} as Filter);
+//список удостоверений
+const storeData: ICertificate[] = [];
 
-      store.getters["certificate/certificates"].forEach((item) => {
-        storeData.push(item);
-      });
-      store.getters["certificateSub/certificatesSub"].forEach((item) => {
-        storeData.push(item);
-      });
-
-      //сортировка по умолчанию - новые сверху
-      storeData.sort((a, b) => {
-        if (a.numCertificate > b.numCertificate) return -1;
-        if (a.numCertificate == b.numCertificate) return 0;
-        if (a.numCertificate < b.numCertificate) return 1;
-      });
-
-      loading.value = false;
-    });
-
-    const certificates = computed(() =>
-      storeData
-        .sort(sorting)
-        .filter((cert) => {
-          if (filter.value.firstname) {
-            return cert.firstname
-              .toLowerCase()
-              .includes(filter.value.firstname.toLowerCase());
-          }
-
-          return true;
-        })
-        .filter((cert) => {
-          if (filter.value.secondname) {
-            return cert.secondname
-              .toLowerCase()
-              .includes(filter.value.secondname.toLowerCase());
-          }
-
-          return true;
-        })
-        .filter((cert) => {
-          if (filter.value.numCertificate) {
-            return cert.numCertificate
-              .toLowerCase()
-              .includes(filter.value.numCertificate.toLowerCase());
-          }
-
-          return true;
-        })
-        .filter((cert) => {
-          if (filter.value.stigmaGeneral) {
-            return cert.stigmaGeneral
-              .toLowerCase()
-              .includes(filter.value.stigmaGeneral.toLowerCase());
-          }
-
-          return true;
-        })
-        .filter((cert) => {
-          if (filter.value.expiration) {
-            return cert.expiration.includes(filter.value.expiration);
-          }
-
-          return true;
-        })
-    );
-
-    function sorting(a, b) {
+const certificates = computed(() =>
+  storeData
+    .sort((a: ICertificate, b: ICertificate): number => {
       switch (filter.value.sortType) {
         case "1":
           if (a.numCertificate > b.numCertificate) return 1;
@@ -152,15 +94,115 @@ export default {
         default:
           break;
       }
-    }
+      return 0;
+    })
+    .filter((cert) => {
+      if (filter.value.firstname) {
+        return cert.firstname
+          .toLowerCase()
+          .includes(filter.value.firstname.toLowerCase());
+      }
 
-    return {
-      certificates,
-      loading,
-      filter,
-      storeData,
-    };
-  },
-  components: { AppPage, AppLoader, CertificatesFilter, CertificatesTable },
-};
+      return true;
+    })
+    .filter((cert) => {
+      if (filter.value.secondname) {
+        return cert.secondname
+          .toLowerCase()
+          .includes(filter.value.secondname.toLowerCase());
+      }
+
+      return true;
+    })
+    .filter((cert) => {
+      if (filter.value.numCertificate) {
+        return cert.numCertificate
+          .toLowerCase()
+          .includes(filter.value.numCertificate.toLowerCase());
+      }
+
+      return true;
+    })
+    .filter((cert) => {
+      if (filter.value.stigmaGeneral) {
+        return cert.stigmaGeneral
+          .toLowerCase()
+          .includes(filter.value.stigmaGeneral.toLowerCase());
+      }
+
+      return true;
+    })
+    .filter((cert) => {
+      if (filter.value.expiration) {
+        return cert.expiration
+          .toString()
+          .includes(filter.value.expiration.toString());
+      }
+
+      return true;
+    })
+);
+
+//загрузка списка удостоверений
+onMounted(async () => {
+  loading.value = true;
+  await store.dispatch("certificate/load");
+  await store.dispatch("certificateSub/load");
+
+  store.getters["certificate/certificates"].forEach((item: ICertificate) => {
+    storeData.push(item);
+  });
+  store.getters["certificateSub/certificatesSub"].forEach(
+    (item: ICertificate) => {
+      storeData.push(item);
+    }
+  );
+
+  //сортировка по умолчанию - новые сверху
+  storeData.sort((a: ICertificate, b: ICertificate): number => {
+    if (a.numCertificate > b.numCertificate) return -1;
+    if (a.numCertificate < b.numCertificate) return 1;
+    return 0;
+  });
+
+  loading.value = false;
+});
+
+// function sorting(a: ICertificate, b: ICertificate) {
+//   switch (filter.value.sortType) {
+//     case "1":
+//       if (a.numCertificate > b.numCertificate) return 1;
+//       if (a.numCertificate == b.numCertificate) return 0;
+//       if (a.numCertificate < b.numCertificate) return -1;
+//       break;
+//     case "2":
+//       if (a.numCertificate > b.numCertificate) return -1;
+//       if (a.numCertificate == b.numCertificate) return 0;
+//       if (a.numCertificate < b.numCertificate) return 1;
+//       break;
+//     case "3":
+//       if (a.firstname > b.firstname) return 1;
+//       if (a.firstname == b.firstname) return 0;
+//       if (a.firstname < b.firstname) return -1;
+//       break;
+//     case "4":
+//       if (a.firstname > b.firstname) return -1;
+//       if (a.firstname == b.firstname) return 0;
+//       if (a.firstname < b.firstname) return 1;
+//       break;
+//     case "5":
+//       if (new Date(a.expiration) > new Date(b.expiration)) return 1;
+//       if (new Date(a.expiration) == new Date(b.expiration)) return 0;
+//       if (new Date(a.expiration) < new Date(b.expiration)) return -1;
+//       break;
+//     case "6":
+//       if (new Date(a.expiration) > new Date(b.expiration)) return -1;
+//       if (new Date(a.expiration) == new Date(b.expiration)) return 0;
+//       if (new Date(a.expiration) < new Date(b.expiration)) return 1;
+//       break;
+
+//     default:
+//       break;
+//   }
+// }
 </script>
